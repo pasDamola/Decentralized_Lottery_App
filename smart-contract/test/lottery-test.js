e76d84fe4bf997e5eaa7bf9e4e64d78e3c918633
pasDamola <oyindadavid18@gmail.com>
2022-02-23 14:25:36 +0100

    updates

--- smart-contract/test/lottery-test.js
+++ smart-contract/test/lottery-test.js
@@ -29,7 +29,6 @@ describe("Lottery Contract", function () {
   
   it("Should allow one account to enter based on successful payment ", async function () {
     const [owner] = await hre.ethers.getSigners();
-    console.log('owner', owner.address);
     const Lottery = await ethers.getContractFactory("Lottery");
     const lottery = await Lottery.deploy();
     await lottery.deployed();
@@ -46,6 +45,28 @@ describe("Lottery Contract", function () {
 
     expect(owner.address).to.equal(players[0])
     expect(players.length).to.equal(1)
+  });
+
+
+  it("Should allow multiple accounts to enter based on successful payment ", async function () {
+    const [owner, secondPerson, thirdPerson] = await hre.ethers.getSigners();
+    const Lottery = await ethers.getContractFactory("Lottery");
+    const lottery = await Lottery.deploy();
+    await lottery.deployed();
+
+    let e;
+
+    try {
+      await lottery.enter({value : ethers.utils.parseEther('0.07')})
+      await lottery.connect(secondPerson).enter({value : ethers.utils.parseEther('0.06')})
+      await lottery.connect(thirdPerson).enter({value : ethers.utils.parseEther('0.06')})
+    } catch (error) {
+      e = error;
+    }
+
+    let players = await lottery.getPlayers();
+
+    expect(players.length).to.equal(3)
   });
 
 });
